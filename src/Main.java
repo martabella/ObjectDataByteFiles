898cb4dc5caa93633ba2ebf12f01fc1e2d5bc961
martabella <mbella@lasalle.cat>
2024-12-13 17:49:28 +0100

    Update Main with text in sout

--- src/Main.java
+++ src/Main.java
@@ -7,36 +7,45 @@ public class Main {
     public static void main(String[] args) {
         Main main = new Main();
         main.inicio();
-
     }
 
     public void inicio(){
+        System.out.println("Comienza programa");
         write();
         read();
-        System.out.println("The change");
+        System.out.println("Programa terminado");
     }
 
     //Writing a byte file (objects.data)
     public void write() {
-
+        System.out.println("Creando array de objetos car");
         Car[] cars = new Car[3];
         cars[0] = new Car("Opel", "Zafira", 5);
         cars[1] = new Car("Ford", "Fiesta", 5);
         cars[2] = new Car("Ford", "Focus", 5);
-        FileOutputStream fileOut = new FileOutputStream("objects.data");
-        ObjectOutputStream output = new ObjectOutputStream(fileOut);
-        for (int i = 0; i < cars.length; i++) {
-            output.writeObject(cars[i]);
-        }
+        System.out.println("Array creado con "+cars.length+" elementos");
+        try(FileOutputStream fileOut = new FileOutputStream("objects.data");
+        ObjectOutputStream output = new ObjectOutputStream(fileOut)) {
+            for (Car car: cars) {
+                output.writeObject(car);
+            }
 
-        System.out.println("Operación de escritura terminada");
+            System.out.println("Operación de escritura en fichero terminada");
+        }catch(Exception e){
+            System.out.println(e);
+        }
     }
     public void read(){
-        FileInputStream fileIn = new FileInputStream("objects.data");
-        ObjectInputStream input = new ObjectInputStream(fileIn);
-        while (true) {
-            Car car = (Car) input.readObject();
-            System.out.println(car.toString());
+        System.out.println("Empezando con operación de lectura de información desde fichero. Mostrando datos....");
+        try(FileInputStream fileIn = new FileInputStream("objects.data");
+        ObjectInputStream input = new ObjectInputStream(fileIn)) {
+            while (true) {
+                Car car = (Car) input.readObject();
+                System.out.println(car.toString());
+            }
+        }catch(Exception e){
+            //System.out.println(e);
         }
+        System.out.println("Operación de lectura terminada");
     }
 }
